7aa3b9ec60aa3a3e3fa74b5727a7b02ce92d9afa
平野　愛翔 <hirano1116@hiranoaishounoMacBook-Pro.local>
2023-12-26 20:23:24 +0900

    実験しました

--- setpartitioning/SPP3.py
+++ setpartitioning/SPP3.py
@@ -6,12 +6,12 @@ from dwave.system.samplers import DWaveSampler
 from dwave.system.composites import EmbeddingComposite
 from functools import partial
 from dwave.embedding.chain_strength import uniform_torque_compensation
-
+import time
 import dwave.inspector
 
-timeP = 1.1
+# timeP = 1.1
 numruns = 1000 #1000
-chain_prm = 1.78
+chain_prm = 1.75
 
 Q = dict()
 with open(f"qubo.csv", "r") as Qreader:
@@ -26,27 +26,27 @@ with open(f"qubo.csv", "r") as Qreader:
             j += 1
         i += 1
 
-chain_stgth = 0
-Q_list = [[abs(Q_list[j][i]) for i in range(n+1)] for j in range(n+1)]
-for i in range(n+1):
-    max_value = max(Q_list[i])
-    if chain_stgth <= max_value:
-        chain_stgth = max_value
+Q_abs = {(i,j): abs(Q[i,j]) for i,j in Q}
+chain_stgth = max(Q_abs.values())
 chain_stgth = int(chain_stgth*chain_prm)
 
 chain_strength = partial(uniform_torque_compensation, prefactor=chain_prm)
 
-solver = "Advantage2_prototype1.1"
+
+solver = "Advantage_system6.3"
 sampler=EmbeddingComposite(DWaveSampler(solver = solver))
-
+time1 = time.time()
 response = sampler.sample_qubo(Q, chain_strength=chain_strength, num_reads=numruns, label='test', return_embedding = True, annealing_time = 50)
-
+print(f"solve time (s) = {time.time() - time1}")
 for s,e,o,chain_break in response.data(['sample', 'energy', 'num_occurrences', 'chain_break_fraction']):
     x_sol = {i for i in range(n) if s[i] > 0.5}
     print(f"energy={e}, occurrence={o}, chain_break={chain_break}\n")
     print(f"sol = {x_sol}")
+    break
 
 t = response.info['timing']
 print(f"t = {t}")
+
+dwave.inspector.show(response)
 
     
